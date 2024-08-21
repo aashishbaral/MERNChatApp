e7e4247b15493ba7816380586ba9c7c393a96a79
Aashish Baral <96362565+aashishbaral@users.noreply.github.com>
2024-08-21 14:34:48 +0545

    changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://mernchatapp-xxuj.onrender.com/",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
